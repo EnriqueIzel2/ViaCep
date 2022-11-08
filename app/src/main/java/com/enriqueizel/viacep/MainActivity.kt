044e4a5a7508db9b2029f2cc136df5e6b605eeda
EnriqueIzel2 <eleaoizel@gmail.com>
2022-11-07 20:55:15 -0400

    feat: create requisition to get cep's data

--- app/src/main/java/com/enriqueizel/viacep/MainActivity.kt
+++ app/src/main/java/com/enriqueizel/viacep/MainActivity.kt
@@ -4,7 +4,15 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.enriqueizel.viacep.api.Api
 import com.enriqueizel.viacep.databinding.ActivityMainBinding
+import com.enriqueizel.viacep.model.Endereco
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
   private lateinit var binding: ActivityMainBinding
@@ -17,5 +25,35 @@ class MainActivity : AppCompatActivity() {
     window.statusBarColor = Color.parseColor("#FF018786")
     val actionBar = supportActionBar
     actionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#FF018786")))
+
+//    configura retrofit
+    val retrofit = Retrofit.Builder()
+      .addConverterFactory(GsonConverterFactory.create())
+      .baseUrl("viacep.com.br/ws/")
+      .build()
+      .create(Api::class.java)
+
+    binding.btnSearchCep.setOnClickListener {
+      val cep = binding.editCep.text.toString()
+
+      if (cep.isEmpty()) {
+        Toast.makeText(this, "Preencha o CEP", Toast.LENGTH_SHORT).show()
+      } else {
+        retrofit.getEndereco(cep).enqueue(object : Callback<Endereco> {
+          override fun onResponse(call: Call<Endereco>, response: Response<Endereco>) {
+            if (response.code() == 200) {
+              val logradouro = response.body()?.logradouro
+              val bairro = response.body()?.bairro
+              val localidade = response.body()?.localidade
+              val uf = response.body()?.uf
+            }
+          }
+
+          override fun onFailure(call: Call<Endereco>, t: Throwable) {
+            TODO("Not yet implemented")
+          }
+        })
+      }
+    }
   }
 }
